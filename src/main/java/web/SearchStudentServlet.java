9d9f18dea1f1ae42a667593566e02d28186e13db
gaoyk <gaoyk19@163.com>
2022-03-25 20:10:27 +0800

    用户搜索

---
+++ src/main/java/web/SearchStudentServlet.java
@@ -0,0 +1,33 @@
+package web;
+
+import pojo.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//@WebServlet(name = "SearchStudentServlet", value = "/SearchStudentServlet")
+public class SearchStudentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getParameter("username");
+//        1. 获取请求得到的参数
+//        2. 发sql语句查询学生信息
+//        3. 保存学生信息到request域中
+//        4. 请求转发到showStudent.jsp页面
+
+        //这里直接使用List数组模拟前两步
+        List<User>userList=new ArrayList<>();
+        for(int i=0;i<10;++i){
+            int t=i+1;
+            userList.add(new User(t,"user"+t,"password"+t,"email"+t+"@163.com"));
+        }
+        request.setAttribute("userList",userList);
+        request.getRequestDispatcher("/.jsp").forward(request,response);
+    }
+
+
+}
---
+++ src/main/webapp/include/main.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gaoyk
+  Date: 2022/3/25
+  Time: 19:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    头部信息<br>
+    主题内容<br>
+    <%@include file="footer.jsp"%>
+    <jsp:include page="footer.jsp"></jsp:include>
+
+</body>
+</html>
