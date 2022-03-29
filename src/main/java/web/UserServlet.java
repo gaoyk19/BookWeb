47a58de28a1730ac9e781a873e99a74032a031cc
gaoyk <gaoyk19@163.com>
2022-03-29 22:15:40 +0800

    使用反射优化if-else代码

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 //@WebServlet(name = "UserServlet", value = "/UserServlet")
 public class UserServlet extends HttpServlet {
@@ -62,10 +64,23 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if("login".equals(request.getParameter("action"))){
-            login(request,response);
-        }else{
-            regist(request,response);
+//        if("login".equals(request.getParameter("action"))){
+//            login(request,response);
+//        }else{
+//            regist(request,response);
+//        }
+        //使用反射优化if-else代码
+        String action = request.getParameter("action");
+        try {
+            Method method=this.getClass().getDeclaredMethod(action,HttpServletRequest.class,HttpServletResponse.class);
+            method.invoke(this,request,response );
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
         }
+
     }
 }
